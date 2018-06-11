cd2e40963cc483db5d4347fb5f5c957abeb5087f
Logesh Kumar <logesh.kumar@krds.fr>
2018-06-11 15:13:05 +0530

    React Router

---
+++ app/components/Home.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var Link = require('react-router-dom').Link;
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="homeContainer">
+                <h1>Battle with your friends</h1>
+                <Link className="button" to="/battle">
+                    Battle
+                </Link>
+            </div>
+        )
+    }
+}
+
+module.exports = Home;
